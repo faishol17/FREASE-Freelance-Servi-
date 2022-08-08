3292bb3d0ad04582ad6c3c3f5c87d11a01870f60
Faishol Susilo Permana <80505984+faishol17@users.noreply.github.com>
2022-08-08 16:59:22 +0700

    tambahan

---
+++ freasefi.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Agu 2022 pada 01.55
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `freasefi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `advantage_service`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.advantage_service: #1932 - Table &#039;freasefi.advantage_service&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.advantage_service: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`advantage_service`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `advantage_user`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.advantage_user: #1932 - Table &#039;freasefi.advantage_user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.advantage_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`advantage_user`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_user`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.detail_user: #1932 - Table &#039;freasefi.detail_user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.detail_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`detail_user`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `experience_user`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.experience_user: #1932 - Table &#039;freasefi.experience_user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.experience_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`experience_user`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.failed_jobs: #1932 - Table &#039;freasefi.failed_jobs&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.migrations: #1932 - Table &#039;freasefi.migrations&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.order: #1932 - Table &#039;freasefi.order&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.order: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`order`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order_status`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.order_status: #1932 - Table &#039;freasefi.order_status&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.order_status: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`order_status`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_resets`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.password_resets: #1932 - Table &#039;freasefi.password_resets&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.personal_access_tokens: #1932 - Table &#039;freasefi.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `service`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.service: #1932 - Table &#039;freasefi.service&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.service: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`service`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sessions`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.sessions: #1932 - Table &#039;freasefi.sessions&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.sessions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`sessions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tagline`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.tagline: #1932 - Table &#039;freasefi.tagline&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.tagline: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`tagline`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_konfirmasi`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.tb_konfirmasi: #1932 - Table &#039;freasefi.tb_konfirmasi&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.tb_konfirmasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`tb_konfirmasi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_transaksi`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.tb_transaksi: #1932 - Table &#039;freasefi.tb_transaksi&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.tb_transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`tb_transaksi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `teams`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.teams: #1932 - Table &#039;freasefi.teams&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.teams: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`teams`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `team_invitations`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.team_invitations: #1932 - Table &#039;freasefi.team_invitations&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.team_invitations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`team_invitations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `team_user`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.team_user: #1932 - Table &#039;freasefi.team_user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.team_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`team_user`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `thumbnail_service`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.thumbnail_service: #1932 - Table &#039;freasefi.thumbnail_service&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.thumbnail_service: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`thumbnail_service`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+-- Kesalahan membaca struktur untuk tabel freasefi.users: #1932 - Table &#039;freasefi.users&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel freasefi.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `freasefi`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
